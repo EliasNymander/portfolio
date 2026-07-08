caaba7a943717b521f478864c1d7845e4c44d231
Elias <elias@boostyourtrade.co.nz>
2026-07-08 14:20:32 +0300

    Add Vercel Web Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { LanguageProvider } from './context/LanguageContext.jsx'
+import { Analytics } from '@vercel/analytics/react';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <LanguageProvider>
       <App />
+      <Analytics />
     </LanguageProvider>
   </StrictMode>,
 )
