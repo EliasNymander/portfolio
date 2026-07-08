01fa65d4347175448c73cb6a086729cbabfbe5d4
Elias <elias@boostyourtrade.co.nz>
2026-07-08 14:25:25 +0300

    Add Vercel Web Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,14 @@ import './index.css'
 import App from './App.jsx'
 import { LanguageProvider } from './context/LanguageContext.jsx'
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <LanguageProvider>
       <App />
       <Analytics />
+      <SpeedInsights/>
     </LanguageProvider>
   </StrictMode>,
 )
